304230583b7eacd8074e4bd02565b254a9f67341
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2023-06-01 00:30:27 +0300

    add missing dependence

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -101,6 +101,7 @@ dependencies {
     val room_version = "2.5.1"
     implementation("androidx.room:room-runtime:$room_version")
     kapt("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
 
     //dagger hilt
     implementation("com.google.dagger:hilt-android:2.44")
