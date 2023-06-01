96f9b600a18c04b45bd465b276699cfc3bda8037
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2023-06-01 21:22:33 +0300

    set default values

--- app/src/main/java/com/red_velvet/yumhub/data/repositories/IngredientRepository.kt
+++ app/src/main/java/com/red_velvet/yumhub/data/repositories/IngredientRepository.kt
@@ -10,19 +10,18 @@ interface IngredientRepository {
 
     suspend fun searchIngredient(
         query: String,
-        sort: String?,
-        intolerances: String?,
-        number :Int?
-    ):IngredientSearchDto
+        sort: String? = "asc",
+        intolerances: String? = "",
+        number: Int? = 1
+    ): IngredientSearchDto
 
     suspend fun getIngredientInformation(
         id: Int,
-        amount: Int,
-        unit: String?,
-
+        amount: Int? = null,
+        unit: String? = "",
     ): IngredientInformationDto
 
     suspend fun getSubstitutesIngredient(
-        ingredientName:String
-    ):IngredientSubstituteDto
+        ingredientName: String
+    ): IngredientSubstituteDto
 }
--- app/src/main/java/com/red_velvet/yumhub/data/repositories/IngredientRepositoryImp.kt
+++ app/src/main/java/com/red_velvet/yumhub/data/repositories/IngredientRepositoryImp.kt
@@ -8,49 +8,44 @@ import com.red_velvet.yumhub.data.remote.dtos.ingredient.IngredientSubstituteDto
 import javax.inject.Inject
 
 class IngredientRepositoryImp @Inject constructor(
-    private val foodService: FoodService,) :IngredientRepository {
+    private val foodService: FoodService,
+) : IngredientRepository {
     override suspend fun searchIngredient(
         query: String,
         sort: String?,
         intolerances: String?,
         number: Int?
-    ): IngredientSearchDto{
-        val response=foodService.searchIngredients(
-            query=query,
-            sort=sort,
-            intolerances=  intolerances,
-            number=  number)
-        if( response.isSuccessful){
-            return  response.body()!!
-        }else{
+    ): IngredientSearchDto {
+        val response = foodService.searchIngredients(
+            query = query,
+            sort = sort,
+        )
+        if (response.isSuccessful) {
+            return response.body()!!
+        } else {
             throw Exception(response.message())
         }
-
     }
 
     override suspend fun getIngredientInformation(
         id: Int,
-        amount: Int,
+        amount: Int?,
         unit: String?
     ): IngredientInformationDto {
-        val response= foodService.getIngredientInformation(
-            id=id ,
-            amount= amount,
-            unit= unit);
-        if(response.isSuccessful){
-            return  response.body()!!
-        }else{
+        val response = foodService.getIngredientInformation(id)
+        if (response.isSuccessful) {
+            return response.body()!!
+        } else {
             throw Exception(response.message())
         }
 
     }
 
     override suspend fun getSubstitutesIngredient(ingredientName: String): IngredientSubstituteDto {
-        val response =foodService.getSubstitutesIngredient(ingredientName=ingredientName)
-        if(response.isSuccessful){
+        val response = foodService.getSubstitutesIngredient(ingredientName = ingredientName)
+        if (response.isSuccessful) {
             return response.body()!!
-        }
-        else{
+        } else {
             throw Exception(response.message())
         }
     }
