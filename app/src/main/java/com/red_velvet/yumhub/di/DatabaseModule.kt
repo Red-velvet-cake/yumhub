164b294b429fc1f1cb2d0b5ef0241f081c82e736
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-06-01 17:16:42 +0300

    add recipe dao provide inject function

--- app/src/main/java/com/red_velvet/yumhub/di/DatabaseModule.kt
+++ app/src/main/java/com/red_velvet/yumhub/di/DatabaseModule.kt
@@ -3,20 +3,31 @@ package com.red_velvet.yumhub.di
 import android.content.Context
 import androidx.room.Room
 import com.red_velvet.yumhub.data.local.FoodDatabase
+import com.red_velvet.yumhub.data.local.daos.RecipeDao
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
 
+    @Singleton
+    @Provides
     fun provideFoodDatabase(@ApplicationContext context: Context) =
-            Room.databaseBuilder(
-                    context,
-                    FoodDatabase::class.java,
-                    "foodDatabase"
-            ).build()
+        Room.databaseBuilder(
+            context,
+            FoodDatabase::class.java,
+            "foodDatabase"
+        ).build()
+
+
+    @Singleton
+    @Provides
+    fun provideRecipeDao(foodDatabase: FoodDatabase) =
+        foodDatabase.recipeDao()
 
 }
