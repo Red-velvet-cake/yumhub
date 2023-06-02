a8b8685b13791a6cf3fa76c456d10b2d3d65611e
Mahmoud Akram Al-Mallahi <nalmamoud303@gmail.com>
2023-06-02 16:19:05 +0300

    create useCases for UserRepo

---
+++ app/src/main/java/com/red_velvet/yumhub/domain/usecases/GetHashUseCase.kt
@@ -0,0 +1,12 @@
+package com.red_velvet.yumhub.domain.usecases
+
+import com.red_velvet.yumhub.data.repositories.UserRepositoryImpl
+import javax.inject.Inject
+
+class GetHashUseCase @Inject constructor(
+    private val repository: UserRepositoryImpl
+) {
+    suspend operator fun invoke() {
+        repository.getHash()
+    }
+}
