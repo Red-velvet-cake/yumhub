0bde1df6e88a8db44b517b286fda9f0019301499
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-06-02 14:14:37 +0300

    add recipes model

---
+++ app/src/main/java/com/red_velvet/yumhub/domain/models/recipes/ConfidenceRange95Percent.kt
@@ -0,0 +1,7 @@
+package com.red_velvet.yumhub.domain.models.recipes
+
+
+data class ConfidenceRange95Percent(
+    val max: Double,
+    val min: Double
+)
---
+++ app/src/main/java/com/red_velvet/yumhub/domain/models/recipes/Nutrient.kt
@@ -0,0 +1,8 @@
+package com.red_velvet.yumhub.domain.models.recipes
+
+
+data class Nutrient(
+    val confidenceRange95Percent: ConfidenceRange95Percent?=null,
+    val unit: String?=null,
+    val value: Double?=null
+)
