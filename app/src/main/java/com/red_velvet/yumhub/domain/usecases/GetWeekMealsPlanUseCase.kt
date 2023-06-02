0f09e3ac83c3af2d534fe571124aeadb20d9efb3
AndrewAboalhana <hana_hany6@yahoo.com>
2023-06-02 12:16:58 +0300

    enhancement on get week meals plan use case

--- app/src/main/java/com/red_velvet/yumhub/domain/usecases/GetWeekMealsPlanUseCase.kt
+++ app/src/main/java/com/red_velvet/yumhub/domain/usecases/GetWeekMealsPlanUseCase.kt
@@ -10,7 +10,7 @@ import javax.inject.Inject
 class GetWeekMealsPlanUseCase @Inject constructor(
     private val mealRepository: MealRepository,
 )  {
-        fun invoke(
+       operator fun invoke(
         fromTimestamp:Long,
         toTimesTamp:Long
     ):Flow<List<MealPlan>>{
