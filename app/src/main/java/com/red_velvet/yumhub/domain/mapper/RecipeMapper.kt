6b37265e02be08dc1ed613d61973a13ea12462dd
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-06-04 19:46:08 +0300

    use extension functions for variables default values

--- app/src/main/java/com/red_velvet/yumhub/domain/mapper/RecipeMapper.kt
+++ app/src/main/java/com/red_velvet/yumhub/domain/mapper/RecipeMapper.kt
@@ -2,12 +2,14 @@ package com.red_velvet.yumhub.domain.mapper
 
 import com.red_velvet.yumhub.data.local.entities.RecipeEntity
 import com.red_velvet.yumhub.domain.models.recipes.Recipe
+import com.red_velvet.yumhub.domain.utils.orEmpty
+import com.red_velvet.yumhub.domain.utils.orZero
 
 fun RecipeEntity.toModel(): Recipe {
     return Recipe(
-        id = this.id ?: 0,
-        title = this.title ?: "",
-        image = this.image ?: "",
-        type = this.type ?: ""
+        id = this.id.orZero(),
+        title = this.title.orEmpty(),
+        image = this.image.orEmpty(),
+        type = this.type.orEmpty()
     )
 }
