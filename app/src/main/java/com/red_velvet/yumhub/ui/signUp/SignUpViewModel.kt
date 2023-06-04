1b4965ffab2de32bd1fe08541605fdfc3ab6f56f
AndrewAboalhana <hana_hany6@yahoo.com>
2023-06-04 18:39:55 +0300

    add validation use cases

--- app/src/main/java/com/red_velvet/yumhub/ui/signUp/SignUpViewModel.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/signUp/SignUpViewModel.kt
@@ -3,6 +3,11 @@ package com.red_velvet.yumhub.ui.signUp
 import androidx.lifecycle.viewModelScope
 import com.red_velvet.yumhub.domain.models.UserInformation
 import com.red_velvet.yumhub.domain.usecases.SaveUserNameAndHashUseCase
+import com.red_velvet.yumhub.domain.usecases.ValidateEmailUseCase
+import com.red_velvet.yumhub.domain.usecases.ValidateFirstName
+import com.red_velvet.yumhub.domain.usecases.ValidateLastNameUseCase
+import com.red_velvet.yumhub.domain.usecases.ValidatePasswordUseCase
+import com.red_velvet.yumhub.domain.usecases.ValidateUsernameUseCase
 import com.red_velvet.yumhub.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -15,13 +20,18 @@ import javax.inject.Inject
 @HiltViewModel
 class SignUpViewModel @Inject constructor(
     private val saveUserInformation: SaveUserNameAndHashUseCase,
+    private val validatePasswordUseCase: ValidatePasswordUseCase,
+    private val validateEmailUseCase: ValidateEmailUseCase,
+    private val validateUsernameUseCase: ValidateUsernameUseCase,
+    private val validateFirstName: ValidateFirstName,
+    private val validateLastNameUseCase: ValidateLastNameUseCase
 ) : BaseViewModel() {
 
     private val _uiState = MutableStateFlow(SignUpUIState())
     val uiState: StateFlow<SignUpUIState> = _uiState
 
     fun onUsernameChange(username: String) {
-        _uiState.update { it.copy(username = username, usernameError = null) }
+        _uiState.update { it.copy(username = username, usernameError = validateUsernameUseCase(username)) }
     }
 
     fun onFirstNameChange(firstName: String) {
@@ -42,44 +52,39 @@ class SignUpViewModel @Inject constructor(
 
     fun onSignUpButtonClicked() {
         val currentState = _uiState.value
-        if (isFormValid(currentState)) {
-            _uiState.update { it.copy(isLoading = true) }
-            viewModelScope.launch {
-                try {
-                    saveUserInformation(currentState.toUserInformation())
-                    _uiState.update { it.copy(isSignUpButtonClicked = true) }
-                } catch (e: Exception) {
-                    _uiState.update { it.copy(errors = e.message.toString()) }
-                } finally {
-                    _uiState.update { it.copy(isLoading = false) }
-                }
-            }
-        } else {
-            updateValidationErrors(currentState)
-        }
+           if (isFormValid(currentState)) {
+               _uiState.update { it.copy(isLoading = true) }
+               viewModelScope.launch {
+                   try {
+                       saveUserInformation(currentState.toUserInformation())
+                       _uiState.update { it.copy(isSignUpButtonClicked = true) }
+                   } catch (e: Exception) {
+                       _uiState.update { it.copy(errors = e.message.toString()) }
+                   } finally {
+                       _uiState.update { it.copy(isLoading = false) }
+                   }
+               }
+           }
     }
-
-
     private fun isFormValid(state: SignUpUIState): Boolean {
+        updateValidationErrors(state)
         return state.username.isNotBlank() &&
                 state.firstName.isNotBlank() &&
                 state.lastName.isNotBlank() &&
                 state.email.isNotBlank() &&
                 state.password.isNotBlank()
     }
-
     private fun updateValidationErrors(state: SignUpUIState) {
         _uiState.update {
             it.copy(
-                usernameError = if (state.username.isBlank()) "Username is required" else null,
-                firstNameError = if (state.firstName.isBlank()) "First name is required" else null,
-                lastNameError = if (state.lastName.isBlank()) "Last name is required" else null,
-                emailError = if (state.email.isBlank()) "Email is required" else null,
-                passwordError = if (state.password.isBlank()) "Password is required" else null
+                usernameError = validateUsernameUseCase(state.username),
+                firstNameError = validateFirstName(state.firstName),
+                lastNameError = validateLastNameUseCase(state.lastName),
+                emailError = validateEmailUseCase(state.email),
+                passwordError = validatePasswordUseCase(state.password)
             )
         }
     }
-
     private fun SignUpUIState.toUserInformation(): UserInformation {
         return UserInformation(
             username = username,
