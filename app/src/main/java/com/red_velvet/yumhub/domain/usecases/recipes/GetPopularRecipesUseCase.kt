04c258140b61b88c476f573a9feb5ef7889f2295
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2023-06-05 15:51:27 +0300

    enhance repository returned model

--- app/src/main/java/com/red_velvet/yumhub/domain/usecases/recipes/GetPopularRecipesUseCase.kt
+++ app/src/main/java/com/red_velvet/yumhub/domain/usecases/recipes/GetPopularRecipesUseCase.kt
@@ -1,10 +1,8 @@
 package com.red_velvet.yumhub.domain.usecases.recipes
 
 import com.red_velvet.yumhub.data.repositories.RecipesRepository
-import com.red_velvet.yumhub.domain.mapper.toModel
 import com.red_velvet.yumhub.domain.models.recipes.Recipe
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetPopularRecipesUseCase @Inject constructor(
@@ -12,11 +10,7 @@ class GetPopularRecipesUseCase @Inject constructor(
 ) {
 
     operator fun invoke(): Flow<List<Recipe>> {
-        return recipesRepositoryImpl.getRecipes("popularity").map { recipeEntities ->
-            recipeEntities.map {
-                it.toModel()
-            }
-        }
+        return recipesRepositoryImpl.getRecipes("popularity")
     }
 
 }
