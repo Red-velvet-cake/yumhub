1e7e17add88015d14451e97026a6befe747f231d
AndrewAboalhana <hana_hany6@yahoo.com>
2023-06-06 20:14:03 +0300

    fix add to meal plan use case to make it use meal plan model

--- app/src/main/java/com/red_velvet/yumhub/domain/usecases/AddToMealPlanUseCase.kt
+++ app/src/main/java/com/red_velvet/yumhub/domain/usecases/AddToMealPlanUseCase.kt
@@ -1,19 +1,18 @@
 package com.red_velvet.yumhub.domain.usecases
 
-import com.red_velvet.yumhub.data.local.entities.MealPlanEntity
 import com.red_velvet.yumhub.data.repositories.MealRepository
-import com.red_velvet.yumhub.domain.mapper.toMealPlanDto
+import com.red_velvet.yumhub.domain.models.MealPlan
 import javax.inject.Inject
 
 class AddToMealPlanUseCase @Inject constructor(
     private val mealRepository: MealRepository,
 ) {
     suspend operator fun invoke(
-        addToMealPlan: MealPlanEntity,
+        addToMealPlan: MealPlan,
         username: String,
         hash: String,
     ) {
         return mealRepository
-            .addToMealPlan(addToMealPlan.toMealPlanDto(), username, hash)
+            .addToMealPlan(addToMealPlan, username, hash)
     }
 }
