c998a7b6081f7dd8fa7e85f098e3e1c7d2d2a5e3
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-06-10 12:03:50 +0300

    enhance use cases functions

--- app/src/main/java/com/red_velvet/yumhub/domain/usecases/recipes/GetPopularRecipesUseCase.kt
+++ app/src/main/java/com/red_velvet/yumhub/domain/usecases/recipes/GetPopularRecipesUseCase.kt
@@ -1,16 +1,23 @@
 package com.red_velvet.yumhub.domain.usecases.recipes
 
-import com.red_velvet.yumhub.data.repositories.RecipesRepository
-import com.red_velvet.yumhub.domain.models.recipes.Recipe
-import kotlinx.coroutines.flow.Flow
+import com.red_velvet.yumhub.domain.RecipesRepository
+import com.red_velvet.yumhub.domain.models.recipes.PopularRecipeEntity
 import javax.inject.Inject
 
 class GetPopularRecipesUseCase @Inject constructor(
     private val recipesRepositoryImpl: RecipesRepository
 ) {
 
-    operator fun invoke(): Flow<List<Recipe>> {
-        return recipesRepositoryImpl.getRecipes("popularity")
+    suspend operator fun invoke(): List<PopularRecipeEntity> {
+        return recipesRepositoryImpl.getPopularRecipesFromLocal().also { savePopularRecipesLocal() }
+    }
+
+    private suspend fun getPopularRecipes(): List<PopularRecipeEntity> {
+        return recipesRepositoryImpl.getPopularRecipes("popularity")
+    }
+
+    private suspend fun savePopularRecipesLocal() {
+        recipesRepositoryImpl.refreshPopularRecipes(getPopularRecipes())
     }
 
 }
