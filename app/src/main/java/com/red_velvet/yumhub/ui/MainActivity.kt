b659039d2bc6864f81ac1919f867ac9673b77a4d
ay0o5h <aayosh553@gmail.com>
2023-06-15 02:16:00 +0300

    remove appbar and bottom in unwanted screen

--- app/src/main/java/com/red_velvet/yumhub/ui/MainActivity.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.red_velvet.yumhub.ui
 
+import android.opengl.Visibility
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.red_velvet.yumhub.R
 import com.red_velvet.yumhub.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,24 +26,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
 
         val navController = findNavController(R.id.nav_host_fragment_content_main)
+        initNavigationDestinationListener()
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
+       binding.bottomNav.setupWithNavController(navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
@@ -50,6 +52,56 @@ class MainActivity : AppCompatActivity() {
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
     }
+    override fun onResume() {
+        super.onResume()
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.SearchFragment,
+                R.id.SplashFragment,
+                R.id.signupFragment,
+            )
+        )
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        binding.bottomNav.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
+        setBottomNavigationVisibility(navController)
+        setNavigationController(navController)
+    }
+
+    private fun setBottomNavigationVisibility(navController: NavController) {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bottomNav.isVisible = destination.id != R.id.SplashFragment
+        }
+    }
+
+    private fun setNavigationController(navController: NavController) {
+        binding.bottomNav.setOnItemSelectedListener { item ->
+            NavigationUI.onNavDestinationSelected(item, navController)
+            navController.popBackStack(item.itemId, inclusive = false)
+            true
+        }
+    }
+
+
+    private fun initNavigationDestinationListener() {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+
+        navController.addOnDestinationChangedListener { _, destination, arguments ->
+            when (destination.id) {
+                R.id.homeFragment,
+                R.id.SearchFragment,
+              -> {
+                    this.supportActionBar?.hide()
+                    binding.bottomNav.isVisible=false
+                }
+                else -> {
+                    binding.bottomNav.isVisible=true
+                    supportActionBar?.show()
+                }
+            }
+        }
+    }
 
 }
