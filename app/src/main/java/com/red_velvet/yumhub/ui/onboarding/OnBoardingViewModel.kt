7ffb8935a08c6211687634bc8078ca72e5724eda
ay0o5h <aayosh553@gmail.com>
2023-06-15 20:33:36 +0300

    on next function

--- app/src/main/java/com/red_velvet/yumhub/ui/onboarding/OnBoardingViewModel.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/onboarding/OnBoardingViewModel.kt
@@ -5,6 +5,7 @@ import com.red_velvet.yumhub.ui.search.SearchRecipeUIState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 
@@ -12,4 +13,18 @@ import javax.inject.Inject
 class OnBoardingViewModel  @Inject constructor() :BaseViewModel<OnBoardingUIState>(OnBoardingUIState()){
     private  val _uiState = MutableStateFlow(OnBoardingUIState())
     val uiState : StateFlow<OnBoardingUIState> = _uiState
+
+    fun onNext(){
+        if(_uiState.value.isSecondTab){
+            _uiState.update {
+                it.copy(isFirstTab = false , isSecondTab = false, isLastTab = true)
+            }
+        }else{
+            _uiState.update {
+                it.copy(isFirstTab = false , isSecondTab = true)
+            }
+
+        }
+
+    }
 }
