8a92accc1dc6bcb6f52b715013b18077a8a0f484
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-06-15 20:57:57 +0300

    complete ui layer for category recipes

---
+++ app/src/main/java/com/red_velvet/yumhub/ui/category/RecipeCategoryUiState.kt
@@ -0,0 +1,11 @@
+package com.red_velvet.yumhub.ui.category
+
+import com.red_velvet.yumhub.ui.base.BaseUiState
+import com.red_velvet.yumhub.ui.base.ErrorUIState
+import com.red_velvet.yumhub.ui.home.RecipeUiState
+
+data class RecipeCategoryUiState(
+    val recipesList: List<RecipeUiState> = emptyList(),
+    val error: ErrorUIState? = null,
+    val isLoading: Boolean = false
+) : BaseUiState()
