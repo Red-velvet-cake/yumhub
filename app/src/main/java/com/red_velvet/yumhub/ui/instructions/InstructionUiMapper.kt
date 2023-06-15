15987c16406afb656738839dcf9b3e0b26d59602
AndrewAboalhana <hana_hany6@yahoo.com>
2023-06-16 02:20:21 +0300

    enhancement on ui mapper

--- app/src/main/java/com/red_velvet/yumhub/ui/instructions/InstructionUiMapper.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/instructions/InstructionUiMapper.kt
@@ -6,12 +6,12 @@ import com.red_velvet.yumhub.domain.models.recipes.StepEntity
 fun List<AnalyzedInstructionsEntity>.toAnalyzedInstructionUiState(): List<InstructionsUIState> {
     return this.map {
         InstructionsUIState(
-            steps = it.steps.toStepsUIState(it.name!!),
+            steps = it.steps.toStepsUIState(it.name),
             name = it.name,
         )
     }
 }
-fun List<StepEntity>.toStepsUIState(name: String): List<StepsUiState> {
+fun List<StepEntity>.toStepsUIState(name: String?): List<StepsUiState> {
     return this.map {
         StepsUiState(
             stepNumber = it.number,
