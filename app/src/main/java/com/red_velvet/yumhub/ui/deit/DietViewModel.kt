ca571218ae1d3519aa5bb5d6a2a28f3f1d31cd6b
ay0o5h <aayosh553@gmail.com>
2023-06-17 01:41:05 +0300

    diet view model

--- app/src/main/java/com/red_velvet/yumhub/ui/deit/DietViewModel.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/deit/DietViewModel.kt
@@ -1,11 +1,17 @@
 package com.red_velvet.yumhub.ui.deit
 
+import android.util.Log
+import androidx.room.util.query
+import com.red_velvet.yumhub.domain.models.recipes.SearchRecipeEntity
 import com.red_velvet.yumhub.domain.usecases.recipes.SearchRecipeUseCase
 import com.red_velvet.yumhub.ui.base.BaseViewModel
+import com.red_velvet.yumhub.ui.base.ErrorUIState
 import com.red_velvet.yumhub.ui.search.SearchRecipeUIState
+import com.red_velvet.yumhub.ui.search.toRecipeSearchResultMapper
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,4 +20,47 @@ class DietViewModel @Inject constructor(
 )  : BaseViewModel<DietUIState>(DietUIState()) {
     private val _uiState = MutableStateFlow(DietUIState())
     val uiState: StateFlow<DietUIState> = _uiState
+
+    fun onSelectType(type:String){
+        if(!ifSameFilterTypeSelected(type)){
+            _uiState.update { it.copy(
+                isLoading = true,
+                type = type,
+            ) }
+            onGetData()
+        }
+    }
+    private fun onGetData(){
+        tryToExecute(
+            callee = {
+                searchRecipeUseCase.invoke(
+                    query="",
+                    sort = _uiState.value.type,
+                    sortDirection = "")
+            },
+            onSuccess = ::onSuccess,
+            onError = ::onError
+        )
+    }
+    private fun ifSameFilterTypeSelected(type :String):Boolean{
+        if(type == _uiState.value.type){
+            _uiState.update { it.copy(
+                isLoading = false,
+                type = "",
+                dietResult = emptyList()) }
+            return true
+        }
+        return  false
+    }
+    private fun onSuccess(recipes: List<SearchRecipeEntity>){
+        val dietResult=  recipes.map { it.toDietResultMapper() }
+        Log.i("AYA",dietResult.toString())
+        _uiState.update { it.copy(
+            dietResult = dietResult,
+            isLoading = false,) }
+    }
+    private fun onError(errorUiState: ErrorUIState) {
+        Log.i("AYA",errorUiState.toString())
+        _state.update { it.copy(error = errorUiState, isLoading = false) }
+    }
 }
