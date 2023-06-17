0f3e6c1fb7d24f1e3eb4c1244e791e7104aacb5e
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2023-06-17 15:08:09 +0300

    merge search fix

--- app/src/main/java/com/red_velvet/yumhub/domain/usecases/recipes/GetIngredientCountUseCase.kt
+++ app/src/main/java/com/red_velvet/yumhub/domain/usecases/recipes/GetIngredientCountUseCase.kt
@@ -5,9 +5,9 @@ import javax.inject.Inject
 
 class GetIngredientCountUseCase @Inject constructor() {
     operator fun invoke(ingredientNumber: List<AnalyzedInstructionsEntity>):Int {
-        return ingredientNumber.flatMap { instruction -> instruction.stepEntities }
+        return ingredientNumber.flatMap { instruction -> instruction.steps }
             .sumOf { step ->
-                step.ingredientsEntity.size
+                step.ingredientsEntity?.size ?: 0
             }
     }
 }
