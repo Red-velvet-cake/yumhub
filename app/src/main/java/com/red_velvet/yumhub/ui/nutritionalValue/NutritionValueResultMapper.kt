c2c9e411ca3f98db0f4682ed69dba4eee4cc424c
ay0o5h <aayosh553@gmail.com>
2023-06-21 02:29:24 +0300

    display data

--- app/src/main/java/com/red_velvet/yumhub/ui/nutritionalValue/NutritionValueResultMapper.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/nutritionalValue/NutritionValueResultMapper.kt
@@ -1,10 +1,6 @@
 package com.red_velvet.yumhub.ui.nutritionalValue
-
 import com.red_velvet.yumhub.domain.models.recipes.GuessNutritionEntity
-import com.red_velvet.yumhub.domain.models.recipes.SearchRecipeEntity
-import com.red_velvet.yumhub.domain.utils.orEmpty
 import com.red_velvet.yumhub.domain.utils.orZero
-import com.red_velvet.yumhub.ui.search.SearchResultUIState
 
 fun GuessNutritionEntity.toNutritionalValueResultUIState(): NutritionalValueResultUIState {
     return NutritionalValueResultUIState(
