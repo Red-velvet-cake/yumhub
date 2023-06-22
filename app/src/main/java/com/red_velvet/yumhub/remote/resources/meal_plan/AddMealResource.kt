a423cc00a91e154abee6040ade57ec9551420d00
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-06-22 15:41:52 +0300

    fix meal plan response error type

--- app/src/main/java/com/red_velvet/yumhub/remote/resources/meal_plan/AddMealResource.kt
+++ app/src/main/java/com/red_velvet/yumhub/remote/resources/meal_plan/AddMealResource.kt
@@ -11,7 +11,7 @@ data class AddMealResource(
     @SerializedName("slot")
     val slot: Int? = 0,
     @SerializedName("type")
-    val type: String? = "",
+    val type: MealPlanItemType = MealPlanItemType.INGREDIENTS,
     @SerializedName("value")
     val valueResource: ValueResource? = ValueResource()
 )
