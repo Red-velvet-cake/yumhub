a69755f25761b3dd764e3e0d88c7478a63895052
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-23 14:49:19 +0300

    change name of fragment xml

--- app/src/main/java/com/red_velvet/yumhub/ui/mealsSuggester/MealsSuggesterFragment.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/mealsSuggester/MealsSuggesterFragment.kt
@@ -3,13 +3,13 @@ package com.red_velvet.yumhub.ui.mealsSuggester
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.viewModels
 import com.red_velvet.yumhub.R
-import com.red_velvet.yumhub.databinding.FragmentMealsSuggesterBinding
+import com.red_velvet.yumhub.databinding.FragmentFoodSuggesterStepOneBinding
 import com.red_velvet.yumhub.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MealsSuggesterFragment:BaseFragment<FragmentMealsSuggesterBinding, MealsSuggesterUiState , MealsSuggesterViewModel>() {
+class MealsSuggesterFragment:BaseFragment<FragmentFoodSuggesterStepOneBinding, MealsSuggesterUiState , MealsSuggesterViewModel>() {
     @LayoutRes
-    override val layoutIdFragment: Int = R.layout.fragment_meals_suggester
+    override val layoutIdFragment: Int = R.layout.fragment_food_suggester_step_one
     override val viewModel: MealsSuggesterViewModel by viewModels()
  }
