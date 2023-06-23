5a2cd55c0c51dd0b1ec53e14cf85e478291b5cbd
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2023-06-23 19:04:29 +0300

    add meal plan interactions methods

--- app/src/main/java/com/red_velvet/yumhub/ui/add_to_meal_plan/OnChooseMealTimeListener.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/add_to_meal_plan/OnChooseMealTimeListener.kt
@@ -3,4 +3,9 @@ package com.red_velvet.yumhub.ui.add_to_meal_plan
 interface OnChooseMealTimeListener {
 
     fun onChooseMealPlanTime(slot: Int)
+
+    fun onAddRecipeToMealPlan()
+
+    fun onUpdateSelectedDate(year: Int, month: Int, dayOfMonth: Int)
+
 }
