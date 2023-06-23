a5720ac6b49d0226d92d4c714ff712d9578bf0a7
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-23 12:02:04 +0300

    map from RecipesByRangeOfCaloriesResource to RecipeEntity

--- app/src/main/java/com/red_velvet/yumhub/repositories/mappers/RecipeEntityMapper.kt
+++ app/src/main/java/com/red_velvet/yumhub/repositories/mappers/RecipeEntityMapper.kt
@@ -4,8 +4,16 @@ import com.red_velvet.yumhub.domain.models.recipes.RecipeEntity
 import com.red_velvet.yumhub.domain.utils.orEmpty
 import com.red_velvet.yumhub.domain.utils.orZero
 import com.red_velvet.yumhub.remote.resources.recipe.RecipeInformationResource
+import com.red_velvet.yumhub.remote.resources.recipe.RecipesByRangeOfCaloriesResource
 
 fun RecipeInformationResource.toRecipeEntity(): RecipeEntity {
+    return RecipeEntity(
+        id = id.orZero(),
+        title = title.orEmpty(),
+        imageUrl = image.orEmpty()
+    )
+}
+fun RecipesByRangeOfCaloriesResource.toEntity(): RecipeEntity{
     return RecipeEntity(
         id = id.orZero(),
         title = title.orEmpty(),
