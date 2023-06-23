fe6eca571f06641fbb97b08b8e3ab3d88ff0ebcc
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-06-23 13:54:31 +0300

    create profile ui state

--- app/src/main/java/com/red_velvet/yumhub/ui/profile/ProfileUiState.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/profile/ProfileUiState.kt
@@ -1,7 +1,11 @@
 package com.red_velvet.yumhub.ui.profile
 
 import com.red_velvet.yumhub.ui.base.BaseUiState
+import com.red_velvet.yumhub.ui.base.ErrorUIState
 
 data class ProfileUiState(
-    val imageUrl: String = ""
+    val imageUrl: String = "",
+    val name: String = "",
+    val error: ErrorUIState? = null,
+    val isLoading: Boolean = false
 ) : BaseUiState
