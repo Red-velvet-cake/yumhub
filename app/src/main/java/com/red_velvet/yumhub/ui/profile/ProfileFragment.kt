15b2f14ef2f340e894a8e3c8347d7b68825f1ca3
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-06-23 15:27:05 +0300

    handle effects in fragment

--- app/src/main/java/com/red_velvet/yumhub/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/profile/ProfileFragment.kt
@@ -1,10 +1,13 @@
 package com.red_velvet.yumhub.ui.profile
 
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.red_velvet.yumhub.R
 import com.red_velvet.yumhub.databinding.FragmentProfileBinding
 import com.red_velvet.yumhub.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ProfileFragment :
@@ -12,8 +15,34 @@ class ProfileFragment :
     override val layoutIdFragment = R.layout.fragment_profile
     override val viewModel: ProfileViewModel by viewModels()
 
-    override fun observeOnUIEffects() {}
+    override fun observeOnUIEffects() {
+        lifecycleScope.launch { viewModel.effect.collectLatest { handleUIEffect(it) } }
+    }
 
-    override fun handleUIEffect(uiEffect: ProfileUiEffect) {}
+    override fun handleUIEffect(uiEffect: ProfileUiEffect) {
+        when (uiEffect) {
+            is ProfileUiEffect.ClickOnPersonalInfo -> navigateToPersonalInfoScreen()
+            is ProfileUiEffect.ClickOnChatBot -> navigateToChatBotScreen()
+            is ProfileUiEffect.ClickOnFavorites -> navigateToFavoritesScreen()
+            is ProfileUiEffect.ClickOnReviewFood -> navigateToReviewFoodScreen()
+            is ProfileUiEffect.ClickOnNutritionalValue -> navigateToNutritionalScreen()
+            is ProfileUiEffect.ClickOnFoodSuggester -> navigateToFoodSuggesterScreen()
+            is ProfileUiEffect.ClickOnLogout -> navigateToLoginScreen()
+        }
+    }
+
+    private fun navigateToPersonalInfoScreen() {}
+
+    private fun navigateToChatBotScreen() {}
+
+    private fun navigateToFavoritesScreen() {}
+
+    private fun navigateToReviewFoodScreen() {}
+
+    private fun navigateToNutritionalScreen() {}
+
+    private fun navigateToFoodSuggesterScreen() {}
+
+    private fun navigateToLoginScreen() {}
 
 }
