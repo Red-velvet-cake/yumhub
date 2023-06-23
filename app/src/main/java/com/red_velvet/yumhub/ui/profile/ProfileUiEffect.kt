9eef450e2f7fd6d36ba1c8f14a0783f5970b57a1
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-06-23 13:54:17 +0300

    create profile ui effect

--- app/src/main/java/com/red_velvet/yumhub/ui/profile/ProfileUiEffect.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/profile/ProfileUiEffect.kt
@@ -3,4 +3,14 @@ package com.red_velvet.yumhub.ui.profile
 import com.red_velvet.yumhub.ui.base.BaseUIEffect
 
 sealed interface ProfileUiEffect : BaseUIEffect {
+    object ClickOnPersonalInfo : ProfileUiEffect
+
+    object ClickOnChatBot : ProfileUiEffect
+
+    object ClickOnReviewFood : ProfileUiEffect
+
+    object ClickOnNutritionalValue : ProfileUiEffect
+
+    object ClickOnFoodSuggester : ProfileUiEffect
+
 }
