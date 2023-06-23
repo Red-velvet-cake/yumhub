ff2925fbb8db9fb4bb3a4657b7215052c3ef9544
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-06-23 14:58:45 +0300

    fix errors after merge

--- app/src/main/java/com/red_velvet/yumhub/ui/add_to_meal_plan/AddToMealPlanUiState.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/add_to_meal_plan/AddToMealPlanUiState.kt
@@ -5,4 +5,4 @@ import com.red_velvet.yumhub.ui.base.BaseUiState
 data class AddToMealPlanUiState(
     val addMealUiState: AddMealUiState = AddMealUiState(),
     val isLoading: Boolean = false
-) : BaseUiState()
+) : BaseUiState
