ad5fb5b1a63a3c4c4003465c2777bc8c8bf34d21
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-23 12:06:14 +0300

    add MealsSuggesterUiState

---
+++ app/src/main/java/com/red_velvet/yumhub/ui/mealsSuggester/MealsSuggesterUiState.kt
@@ -0,0 +1,7 @@
+package com.red_velvet.yumhub.ui.mealsSuggester
+
+import com.red_velvet.yumhub.ui.base.BaseUiState
+
+data class MealsSuggesterUiState(
+    val calories : Double= 0.0
+):BaseUiState()
