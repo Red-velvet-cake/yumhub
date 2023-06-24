c95926b1bff2bcff5b08f680e1272e04ecb4159c
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-24 14:42:30 +0300

    add data class to click on activity level

--- app/src/main/java/com/red_velvet/yumhub/ui/mealsSuggester/MealsSuggesterUiEffect.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/mealsSuggester/MealsSuggesterUiEffect.kt
@@ -1,5 +1,6 @@
 package com.red_velvet.yumhub.ui.mealsSuggester
 
 sealed interface MealsSuggesterUiEffect {
-    data class clickOnGenderSelector(val gender: String):MealsSuggesterUiEffect
+    data class ClickOnGenderSelector(val gender: String):MealsSuggesterUiEffect
+    data class ClickOnActivityLevelSelector(val activityLevel: String): MealsSuggesterUiEffect
 }
