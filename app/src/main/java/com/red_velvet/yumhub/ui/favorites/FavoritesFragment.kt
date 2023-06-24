1e60534d918f27f0e5c0e2336da841d4914e0cc6
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-06-24 14:53:36 +0300

    handle navigation to recipe details from favorites screen

--- app/src/main/java/com/red_velvet/yumhub/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/favorites/FavoritesFragment.kt
@@ -1,10 +1,15 @@
 package com.red_velvet.yumhub.ui.favorites
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.red_velvet.yumhub.R
 import com.red_velvet.yumhub.databinding.FragmentFavoritesBinding
 import com.red_velvet.yumhub.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class FavoritesFragment :
@@ -12,12 +17,27 @@ class FavoritesFragment :
     override val layoutIdFragment = R.layout.fragment_favorites
     override val viewModel: FavoritesViewModel by viewModels()
 
-    override fun observeOnUIEffects() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val favoriteRecipesAdapter = FavoriteRecipesAdapter(emptyList(), viewModel)
+        binding.favoritesRecycler.adapter = favoriteRecipesAdapter
+    }
+
+    override fun observeOnUIEffects() {
+        lifecycleScope.launch { viewModel.effect.collect { handleUIEffect(it) } }
     }
 
     override fun handleUIEffect(uiEffect: FavoritesUiEffect) {
+        when (uiEffect) {
+            is FavoritesUiEffect.ClickOnRecipe -> navigateToRecipeDetails(uiEffect.id)
+        }
+    }
 
+    private fun navigateToRecipeDetails(id: Int) {
+        val action =
+            FavoritesFragmentDirections.actionFavoritesFragmentToRecipeInformationFragment(id)
+        findNavController().navigate(action)
     }
 
 }
