e3c7d643f254e4eed04a17151e90103e25fc5042
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-06-24 12:39:31 +0300

    change layout name

--- app/src/main/java/com/red_velvet/yumhub/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/favorites/FavoritesFragment.kt
@@ -2,13 +2,13 @@ package com.red_velvet.yumhub.ui.favorites
 
 import androidx.fragment.app.viewModels
 import com.red_velvet.yumhub.R
-import com.red_velvet.yumhub.databinding.FragmentFavoriteBinding
+import com.red_velvet.yumhub.databinding.FragmentFavoritesBinding
 import com.red_velvet.yumhub.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class FavoritesFragment :
-    BaseFragment<FragmentFavoriteBinding, FavoritesUiState, FavoritesUiEffect, FavoritesViewModel>() {
+    BaseFragment<FragmentFavoritesBinding, FavoritesUiState, FavoritesUiEffect, FavoritesViewModel>() {
     override val layoutIdFragment = R.layout.fragment_favorites
     override val viewModel: FavoritesViewModel by viewModels()
 
