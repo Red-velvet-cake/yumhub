436f59339d4de0f7c8b81550367f8af579413364
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-24 14:15:02 +0300

    add ui state of the first fragment

--- app/src/main/java/com/red_velvet/yumhub/ui/mealsSuggester/MealsSuggesterUiState.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/mealsSuggester/MealsSuggesterUiState.kt
@@ -3,5 +3,7 @@ package com.red_velvet.yumhub.ui.mealsSuggester
 import com.red_velvet.yumhub.ui.base.BaseUiState
 
 data class MealsSuggesterUiState(
-    val calories : Double= 0.0
+    val isLoading: Boolean = true,
+    val gender: String = "Male",
+    val activityLevel: String = "",
 ):BaseUiState()
