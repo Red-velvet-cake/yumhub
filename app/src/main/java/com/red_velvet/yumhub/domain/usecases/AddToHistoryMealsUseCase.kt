71e56feb4b10236a6c747cb836914c9ab11f7f31
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-06-25 00:05:45 +0300

    make history usecases being success

--- app/src/main/java/com/red_velvet/yumhub/domain/usecases/AddToHistoryMealsUseCase.kt
+++ app/src/main/java/com/red_velvet/yumhub/domain/usecases/AddToHistoryMealsUseCase.kt
@@ -1,32 +1,74 @@
 package com.red_velvet.yumhub.domain.usecases
 
+import android.util.Log
 import com.red_velvet.yumhub.domain.models.HistoryMealEntity
 import com.red_velvet.yumhub.domain.repositories.MealRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import javax.inject.Inject
+
+//class AddToHistoryMealsUseCase @Inject constructor(
+//    private val mealRepository: MealRepository,
+//    val getHistoryMealsUsecase: GetHistoryMealsUsecase,
+//) {
+//    suspend operator fun invoke(
+//        historyMealEntity: HistoryMealEntity
+//    ) {
+//        var historyList = getHistoryMealsUsecase()
+//        if (historyList.contains(historyMealEntity)) {
+//            raiseItemToTopIfFound(historyList, historyMealEntity)
+//        } else {
+//            addToHistory(historyList, historyMealEntity)
+//        }
+//    }
+//
+//    private suspend fun raiseItemToTopIfFound(
+//        historyList: MutableList<HistoryMealEntity>, historyMealEntity: HistoryMealEntity
+//    ) {
+//        historyList.remove(historyMealEntity)
+//        addToHistory(historyList, historyMealEntity)
+//    }
+//
+//    private suspend fun addToHistory(
+//        historyList: MutableList<HistoryMealEntity>, historyMealEntity: HistoryMealEntity
+//    ) {
+//        historyList.add(historyMealEntity)
+//        mealRepository.addToHistoryMeals(historyList)
+//    }
+//}
 
 class AddToHistoryMealsUseCase @Inject constructor(
     private val mealRepository: MealRepository,
-    val getHistoryMealsUsecase: GetHistoryMealsUsecase,
+    private val getHistoryMealsUsecase: GetHistoryMealsUsecase,
+) {
+    suspend operator fun invoke(historyMealEntity: HistoryMealEntity) {
+        try {
+            val historyList = getHistoryMealsUsecase().first().toMutableList()
 
-    ) {
-    suspend operator fun invoke(
-        historyMealEntity: HistoryMealEntity
-    ) {
-        val historyList = getHistoryMealsUsecase().toMutableList()
-        if (historyList.contains(historyMealEntity)) {
-            raiseItemToTopIfFound(historyList, historyMealEntity)
-        } else {
-            historyList.add(historyMealEntity)
-            mealRepository.addToHistoryMeals(historyList)
+            if (historyList.contains(historyMealEntity)) {
+                raiseItemToTopIfFound(historyList, historyMealEntity)
+            } else {
+                historyList.add(historyMealEntity)
+                mealRepository.addToHistoryMeals(historyList)
+            }
+        } catch (e: Exception) {
+            // Handle exception
+            Log.e("TAG", "Error: $e")
         }
     }
+
 
     private suspend fun raiseItemToTopIfFound(
         historyList: MutableList<HistoryMealEntity>, historyMealEntity: HistoryMealEntity
     ) {
         historyList.remove(historyMealEntity)
-        historyList.add(historyMealEntity)
-//        historyList.sortedBy { it.date }
+        addToHistory(historyList, historyMealEntity)
+    }
+
+    private suspend fun addToHistory(
+        historyList: MutableList<HistoryMealEntity>, historyMealEntity: HistoryMealEntity
+    ) {
+        historyList.add(0, historyMealEntity)
         mealRepository.addToHistoryMeals(historyList)
     }
 }
--- app/src/main/java/com/red_velvet/yumhub/domain/usecases/GetHistoryMealsUsecase.kt
+++ app/src/main/java/com/red_velvet/yumhub/domain/usecases/GetHistoryMealsUsecase.kt
@@ -2,12 +2,13 @@ package com.red_velvet.yumhub.domain.usecases
 
 import com.red_velvet.yumhub.domain.models.HistoryMealEntity
 import com.red_velvet.yumhub.domain.repositories.MealRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetHistoryMealsUsecase @Inject constructor(
     private val mealRepository: MealRepository,
 ) {
-    operator fun invoke(): List<HistoryMealEntity> {
+    suspend operator fun invoke(): Flow<List<HistoryMealEntity>> {
         return mealRepository.getHistoryMeals()
     }
 }
