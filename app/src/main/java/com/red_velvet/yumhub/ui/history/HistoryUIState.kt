49773fa0d14a2c93508073f4b300f740243bde74
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-06-25 02:25:08 +0300

    complete history viewmodel

--- app/src/main/java/com/red_velvet/yumhub/ui/history/HistoryUIState.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/history/HistoryUIState.kt
@@ -4,7 +4,7 @@ import com.red_velvet.yumhub.ui.base.BaseUiState
 import com.red_velvet.yumhub.ui.base.ErrorUIState
 
 data class HistoryUIState(
-    val searchResult: List<HistoryItemUIState> = emptyList(),
+    val historyItems: List<HistoryItemUIState> = emptyList(),
     val isResultIsEmpty: Boolean = false,
     val error: ErrorUIState? = null,
 ) : BaseUiState {
---
+++ app/src/main/java/com/red_velvet/yumhub/ui/history/HistoryViewModel.kt
@@ -0,0 +1,55 @@
+package com.red_velvet.yumhub.ui.history
+
+import androidx.lifecycle.viewModelScope
+import com.red_velvet.yumhub.domain.models.HistoryMealEntity
+import com.red_velvet.yumhub.domain.usecases.GetHistoryMealsUsecase
+import com.red_velvet.yumhub.ui.base.BaseViewModel
+import com.red_velvet.yumhub.ui.base.ErrorUIState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HistoryViewModel @Inject constructor(
+    private val getHistoryMeals: GetHistoryMealsUsecase,
+) : BaseViewModel<HistoryUIState, HistoryUIEffect>(HistoryUIState()), HistoryInteractionListener {
+
+    private val _uiState = MutableStateFlow(HistoryUIState())
+    val uiState: StateFlow<HistoryUIState> = _uiState
+
+    init {
+        getHistoryItems()
+    }
+
+    private fun getHistoryItems() {
+        tryToExecute(
+            callee = getHistoryMeals::invoke, onSuccess = ::onSuccess, onError = ::onError
+        )
+    }
+
+    private fun onSuccess(items: Flow<List<HistoryMealEntity>>) {
+        viewModelScope.launch {
+            items.collect { it ->
+                val historyItems = it.map { it.toUiState() }
+                _uiState.update {
+                    it.copy(
+                        historyItems = historyItems, isResultIsEmpty = historyItems.isEmpty()
+                    )
+                }
+            }
+        }
+    }
+
+    private fun onError(errorUiState: ErrorUIState) {
+        _state.update { it.copy(error = errorUiState) }
+    }
+
+    override fun onHistoryItemClicked(itemId: Int) {
+        viewModelScope.launch { _effect.emit(HistoryUIEffect.ClickOnItem(itemId)) }
+    }
+
+}
