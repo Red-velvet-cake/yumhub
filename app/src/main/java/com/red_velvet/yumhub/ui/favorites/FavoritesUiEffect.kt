742b08bc2ab5612c15c86dd791263bfed39395a4
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-06-24 14:52:38 +0300

    create favorites ui effect

--- app/src/main/java/com/red_velvet/yumhub/ui/favorites/FavoritesUiEffect.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/favorites/FavoritesUiEffect.kt
@@ -2,4 +2,7 @@ package com.red_velvet.yumhub.ui.favorites
 
 import com.red_velvet.yumhub.ui.base.BaseUIEffect
 
-sealed class FavoritesUiEffect : BaseUIEffect {}
+sealed class FavoritesUiEffect : BaseUIEffect {
+
+    data class ClickOnRecipe(val id: Int) : FavoritesUiEffect()
+}
