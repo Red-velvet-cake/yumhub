bd84347c386b9406c0c30df7eb3d2b6f43393049
mustafayassi <must3f3yassin@gmail.com>
2023-06-26 01:11:10 +0300

    create find your meal adapter

---
+++ app/src/main/java/com/red_velvet/yumhub/ui/fnd_meal/FindYourMealAdapter.kt
@@ -0,0 +1,17 @@
+package com.red_velvet.yumhub.ui.fnd_meal
+
+import com.red_velvet.yumhub.R
+import com.red_velvet.yumhub.domain.models.recipes.SearchRecipeEntity
+import com.red_velvet.yumhub.ui.base.BaseAdapter
+import com.red_velvet.yumhub.ui.base.BaseInteractionListener
+
+class FindYourMealAdapter(item: List<SearchRecipeEntity>, listener: FindYourMealInteractionListener) :
+    BaseAdapter<SearchRecipeEntity>(item, listener) {
+    override val layoutId: Int = R.layout.item_search_card
+}
+
+interface FindYourMealInteractionListener : BaseInteractionListener {
+
+    fun onRecipeFindYourMealResultClicked(recipeId: Int)
+
+}
