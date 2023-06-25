ad7fce4fca23c1085f46e77a4c02ce654f44e57d
ay0o5h <aayosh553@gmail.com>
2023-06-25 13:24:48 +0300

    add QuickAnswerLocalDto

---
+++ app/src/main/java/com/red_velvet/yumhub/local/entities/QuickAnswerLocalDto.kt
@@ -0,0 +1,12 @@
+package com.red_velvet.yumhub.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "QUICK_ANSWER_TABLE")
+data class QuickAnswerLocalDto(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val qsn: String,
+    val answer: String,
+    val image: Int
+)
