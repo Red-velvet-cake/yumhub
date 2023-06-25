da3ae026477006bc1b25f1ea1dc144af304dbce2
ay0o5h <aayosh553@gmail.com>
2023-06-25 23:41:15 +0300

    chat bot ui state

---
+++ app/src/main/java/com/red_velvet/yumhub/ui/chatbot/ChatBotResultUIState.kt
@@ -0,0 +1,7 @@
+package com.red_velvet.yumhub.ui.chatbot
+
+data class ChatBotResultUIState(
+    val answer: String,
+    val image: String,
+    val qsn: String,
+)
--- app/src/main/java/com/red_velvet/yumhub/ui/chatbot/ChatBotUIState.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/chatbot/ChatBotUIState.kt
@@ -4,6 +4,8 @@ import com.red_velvet.yumhub.ui.base.BaseUiState
 import com.red_velvet.yumhub.ui.base.ErrorUIState
 
 data class ChatBotUIState(
+    val quickAnswer: String = "",
+    val quickAnswerResult: List<ChatBotResultUIState> = emptyList(),
     val isLoading: Boolean = false,
     val error: ErrorUIState? = null,
     ):BaseUiState
