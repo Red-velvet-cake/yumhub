cf4fe3ba053100e731473775bf0d71e00018c4e5
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-06-25 03:02:24 +0300

    complete data layer for home slider list

--- app/src/main/java/com/red_velvet/yumhub/repositories/datasources/LocalDataSource.kt
+++ app/src/main/java/com/red_velvet/yumhub/repositories/datasources/LocalDataSource.kt
@@ -5,6 +5,7 @@ import com.red_velvet.yumhub.local.entities.HealthyRecipeLocalDto
 import com.red_velvet.yumhub.local.entities.MealPlanLocalDto
 import com.red_velvet.yumhub.local.entities.PopularRecipeLocalDto
 import com.red_velvet.yumhub.local.entities.QuickRecipeLocalDto
+import com.red_velvet.yumhub.local.entities.SliderItemLocalDto
 import kotlinx.coroutines.flow.Flow
 
 interface LocalDataSource {
@@ -26,5 +27,7 @@ interface LocalDataSource {
     fun getQuickRecipes(): Flow<List<QuickRecipeLocalDto>>
 
     suspend fun insertQuickRecipes(quickRecipes: List<QuickRecipeLocalDto>)
+
+    suspend fun getHomeSliderImagesList(): List<SliderItemLocalDto>
 
 }
