cb486b3e54811d4b58a511ee36eebc654754a6c4
mustafayassi <must3f3yassin@gmail.com>
2023-06-26 01:13:36 +0300

    create find your meal result ui state

---
+++ app/src/main/java/com/red_velvet/yumhub/ui/fnd_meal/FindYourMealResultUiState.kt
@@ -0,0 +1,9 @@
+package com.red_velvet.yumhub.ui.fnd_meal
+
+data class FindYourMealResultUiState(
+    val id: Int,
+    val title: String,
+    val image: String,
+    val ingredientNumber: Int,
+    val readyInMinutes: String,
+)
