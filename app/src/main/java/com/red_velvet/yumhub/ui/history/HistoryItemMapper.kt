c1200ba9479d51f84aef5549f6bb83931027d2f0
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-06-25 03:24:27 +0300

    make final touchs

--- app/src/main/java/com/red_velvet/yumhub/ui/history/HistoryItemMapper.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/history/HistoryItemMapper.kt
@@ -4,8 +4,8 @@ import com.red_velvet.yumhub.domain.models.HistoryMealEntity
 import com.red_velvet.yumhub.domain.utils.orEmpty
 import com.red_velvet.yumhub.domain.utils.orZero
 
-fun HistoryMealEntity.toUiState(): HistoryItemUIState {
-    return HistoryItemUIState(
+fun HistoryMealEntity.toUiState(): HistoryUIState.HistoryItemUIState {
+    return HistoryUIState.HistoryItemUIState(
         id = id.orZero(),
         title = title.orEmpty(),
         description = description.orEmpty(),
