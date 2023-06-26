784bb9595596922f019b140882e3d02943ea605a
ay0o5h <aayosh553@gmail.com>
2023-06-27 01:00:07 +0300

    change ChatBotResultUIStateMapper

--- app/src/main/java/com/red_velvet/yumhub/ui/chatbot/ChatBotResultUIMapper.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/chatbot/ChatBotResultUIMapper.kt
@@ -3,12 +3,19 @@ package com.red_velvet.yumhub.ui.chatbot
 import com.red_velvet.yumhub.domain.models.recipes.QuickAnswerEntity
 import com.red_velvet.yumhub.domain.utils.orEmpty
 
-fun List<QuickAnswerEntity>.toMapper(): List<ChatBotResultUIState>{
-    return this.map {
-        ChatBotResultUIState(
-            qsn = it.qsn.orEmpty(),
-            image = it.image.orEmpty(),
-            answer = it.answer.orEmpty(),
-        )
+fun List<QuickAnswerEntity>.toMapper(): List<ChatBotResultUIState> {
+    val result: MutableList<ChatBotResultUIState> = mutableListOf()
+    for (chatBotResult in this) {
+        if (chatBotResult.qsn.isNotEmpty()) {
+            val questionTest = ChatBotResultUIState(true, chatBotResult.qsn, "")
+            result.add(questionTest)
+        }
+
+        if (chatBotResult.answer.isNotEmpty()) {
+            val answerTest = ChatBotResultUIState(false, chatBotResult.answer, chatBotResult.image)
+            result.add(answerTest)
+        }
     }
+
+    return result
 }
