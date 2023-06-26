da1396bd9299decb568cfb8042a85d6469a302f3
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2023-06-26 12:48:46 +0300

    use state inherited variable

--- app/src/main/java/com/red_velvet/yumhub/ui/onboarding/OnBoardingViewModel.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/onboarding/OnBoardingViewModel.kt
@@ -2,8 +2,6 @@ package com.red_velvet.yumhub.ui.onboarding
 
 import com.red_velvet.yumhub.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
@@ -13,16 +11,13 @@ class OnBoardingViewModel @Inject constructor() :
     BaseViewModel<OnBoardingUIState, OnBoardingUIEffect>(OnBoardingUIState()),
     OnBoardingInteractionListener {
 
-    private val _uiState = MutableStateFlow(OnBoardingUIState())
-    val uiState: StateFlow<OnBoardingUIState> = _uiState
-
     override fun onClickNext() {
-        if (_uiState.value.isSecondTab) {
-            _uiState.update {
+        if (_state.value.isSecondTab) {
+            _state.update {
                 it.copy(isFirstTab = false, isSecondTab = false, isLastTab = true)
             }
         } else {
-            _uiState.update {
+            _state.update {
                 it.copy(isFirstTab = false, isSecondTab = true)
             }
         }
