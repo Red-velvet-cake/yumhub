3e7ad868d8cf5f40ecdec3a5fc2c68a301884136
mustafayassi <must3f3yassin@gmail.com>
2023-06-26 12:39:44 +0300

    delete find your meal  result ui state

--- app/src/main/java/com/red_velvet/yumhub/ui/fnd_meal/FindYourMealResultUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.red_velvet.yumhub.ui.fnd_meal
-
-data class FindYourMealResultUiState(
-    val id: Int,
-    val title: String,
-    val image: String,
-    val ingredientNumber: Int,
-    val readyInMinutes: String,
-)
