efd324e5084bc07e3e993c43788df5374737d42d
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-27 21:33:43 +0300

    handle view model

--- app/src/main/java/com/red_velvet/yumhub/ui/mealsSuggester/MealsSuggesterStep1ViewModel.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/mealsSuggester/MealsSuggesterStep1ViewModel.kt
@@ -5,10 +5,14 @@ import androidx.lifecycle.viewModelScope
 import com.red_velvet.yumhub.domain.models.FoodSystemEntity
 import com.red_velvet.yumhub.domain.models.recipes.RecipeEntity
 import com.red_velvet.yumhub.domain.usecases.CalculateNeededCaloriesUseCase
+import com.red_velvet.yumhub.domain.utils.orEmpty
+import com.red_velvet.yumhub.domain.utils.orZero
 import com.red_velvet.yumhub.ui.base.BaseViewModel
 import com.red_velvet.yumhub.ui.base.ErrorUIState
 import com.red_velvet.yumhub.ui.mealsSuggester.mealsSuggesterStep3.SuggestedMealsInteractionListener
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -30,7 +34,7 @@ class MealsSuggesterStep1ViewModel @Inject constructor(
             )
         }
     }
-     fun updateActivityLevel(activityLevel: String)
+     fun updateActivityLevel(activityLevel: Int)
     {
         _state.update { it.copy(activityLevel = activityLevel) }
         viewModelScope.launch {
@@ -40,7 +44,6 @@ class MealsSuggesterStep1ViewModel @Inject constructor(
                 )
             )
         }
-        Log.i("jalal", _state.value.toString())
     }
 
 
@@ -50,55 +53,58 @@ class MealsSuggesterStep1ViewModel @Inject constructor(
     }
 
     fun updateTall(tall: String) {
+        if (tall!="")
         _state.update { it.copy(tall = tall.toInt()) }
+        else
+            _state.update { it.copy(tall = -1) }
     }
 
     fun updateAge(age: String) {
+        if (age!="")
         _state.update { it.copy(age = age.toInt()) }
+        else
+            _state.update { it.copy(age = -1) }
     }
 
     fun updateWeight(weight: String) {
+        if (weight!="")
         _state.update { it.copy(weight = weight.toInt()) }
+        else
+            _state.update { it.copy(weight = -1) }
     }
-
-    init {
-        onFetchData()
-    }
-
-    private fun onFetchData() {
-        tryToExecute({calculateNeededCaloriesUseCase.calculateNeededCalories(
-            FoodSystemEntity(
-                age = 27,
-                weight = 80,
-                height = 187,
-                activityLevel = 2,
-                gender = "Male",
-                goal = "Loss Weight"
-            )
-        )},
-            onSuccess = ::onSuccessFetchData,
-            onError = ::onError
-        )
-    }
-
     override fun onMealClick(id: Int) {
 
     }
     fun onSuccessFetchData(meals: List<RecipeEntity>)
     {
-        _state.update { it.copy(isLoading = false, meals = meals.toSuggestedMeals()) }
-        Log.i("jalal",_state.value.toString())
+        _state.update { it.copy( meals = meals.toSuggestedMeals()) }
+        Log.i("jalal","your state is ${_state.value}")
 
     }
     fun onError(throwable: ErrorUIState)
     {
-
+        _state.update { it.copy(error = throwable) }
     }
-    fun onNextButtonClicked()
-    {
-        viewModelScope.launch { _effect.emit(MealsSuggesterStep1UiEffect.OnNextClicked) }
-    }
-
-
+     fun onNextButtonClicked(type: String) {
+         if (type == "stepTwo") {
+             tryToExecute(
+                 {
+                     calculateNeededCaloriesUseCase.calculateNeededCalories(
+                         FoodSystemEntity(
+                             age = _state.value.age.orZero(),
+                             weight = _state.value.weight.orZero(),
+                             height = _state.value.tall.orZero(),
+                             activityLevel = _state.value.activityLevel.orZero(),
+                             gender = _state.value.gender.orEmpty(),
+                             goal = _state.value.goal.orEmpty()
+                         )
+                     )
+                 },
+                 onSuccess = ::onSuccessFetchData,
+                 onError = ::onError
+             )
+         }
+         viewModelScope.launch { _effect.emit(MealsSuggesterStep1UiEffect.OnNextClicked(type)) }
+     }
 
 }
