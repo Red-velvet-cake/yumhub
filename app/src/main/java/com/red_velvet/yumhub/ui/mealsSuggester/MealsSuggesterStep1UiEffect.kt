e8461f9daeea545f81c709a69f2a53b49b2a66ae
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-27 21:29:03 +0300

    chang type of inner class MealsSuggesterStep1UiEffect

--- app/src/main/java/com/red_velvet/yumhub/ui/mealsSuggester/MealsSuggesterStep1UiEffect.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/mealsSuggester/MealsSuggesterStep1UiEffect.kt
@@ -5,8 +5,8 @@ import com.red_velvet.yumhub.ui.base.BaseUIEffect
 
 sealed interface MealsSuggesterStep1UiEffect : BaseUIEffect {
     data class ClickOnGenderSelector(val gender: String) : MealsSuggesterStep1UiEffect
-    data class ClickOnActivityLevelSelector(val activityLevel: String) : MealsSuggesterStep1UiEffect
-    object OnNextClicked : MealsSuggesterStep1UiEffect
+    data class ClickOnActivityLevelSelector(val activityLevel: Int) : MealsSuggesterStep1UiEffect
+    data class OnNextClicked(val type: String) : MealsSuggesterStep1UiEffect
 
     data class ClickOnGoalSelector(val goal: String) : MealsSuggesterStep1UiEffect
 
