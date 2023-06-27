8883f9d710c69c4f9f308ef154632dcac261bddf
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-27 21:30:45 +0300

    change in data type of ui state

--- app/src/main/java/com/red_velvet/yumhub/ui/mealsSuggester/MealsSuggesterStep1UiState.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/mealsSuggester/MealsSuggesterStep1UiState.kt
@@ -2,21 +2,23 @@ package com.red_velvet.yumhub.ui.mealsSuggester
 
 import com.red_velvet.yumhub.domain.models.recipes.RecipeEntity
 import com.red_velvet.yumhub.ui.base.BaseUiState
+import com.red_velvet.yumhub.ui.base.ErrorUIState
 
 data class MealsSuggesterStep1UiState(
     val isLoading: Boolean = true,
     val gender: String = "Male",
-    val activityLevel: String = "",
+    val activityLevel: Int? = 0,
     val goal: String? = "",
     val weight: Int? = 0,
     val tall: Int? = 0,
     val age: Int? = 0,
     val calories: Int? = 0,
-    val meals: List<SuggestedMeals>? = emptyList()
+    val meals: List<SuggestedMeals>? = emptyList(),
+    val error:ErrorUIState? = null
 ) : BaseUiState {
     data class SuggestedMeals(
-        val imageUrl: String,
-        val title: String,
+        val imageUrl: String = "",
+        val title: String = "",
     )
 }
 fun RecipeEntity.toSuggestedMeals(): MealsSuggesterStep1UiState.SuggestedMeals{
