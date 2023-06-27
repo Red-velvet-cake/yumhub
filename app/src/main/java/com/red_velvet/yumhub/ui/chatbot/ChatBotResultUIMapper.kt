63888084a757906a31b7cae304bfd8eb7bad1ae0
ay0o5h <aayosh553@gmail.com>
2023-06-27 11:27:19 +0300

    improve chatbot ui mapper

--- app/src/main/java/com/red_velvet/yumhub/ui/chatbot/ChatBotResultUIMapper.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/chatbot/ChatBotResultUIMapper.kt
@@ -4,18 +4,10 @@ import com.red_velvet.yumhub.domain.models.recipes.QuickAnswerEntity
 import com.red_velvet.yumhub.domain.utils.orEmpty
 
 fun List<QuickAnswerEntity>.toMapper(): List<ChatBotResultUIState> {
-    val result: MutableList<ChatBotResultUIState> = mutableListOf()
-    for (chatBotResult in this) {
-        if (chatBotResult.qsn.isNotEmpty()) {
-            val questionTest = ChatBotResultUIState(true, chatBotResult.qsn, "")
-            result.add(questionTest)
-        }
-
-        if (chatBotResult.answer.isNotEmpty()) {
-            val answerTest = ChatBotResultUIState(false, chatBotResult.answer, chatBotResult.image)
-            result.add(answerTest)
-        }
+    return this.flatMap { chatBotResult ->
+        listOfNotNull(
+            chatBotResult.qsn.takeIf { it.isNotEmpty() }?.let { ChatBotResultUIState(true, it, "") },
+            chatBotResult.answer.takeIf { it.isNotEmpty() }?.let { ChatBotResultUIState(false, it, chatBotResult.image) }
+        )
     }
-
-    return result
 }
