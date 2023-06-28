7bc029c7d0307450ac7f726632ddf0498d8dd7d2
mustafayassi <must3f3yassin@gmail.com>
2023-06-28 19:18:48 +0300

    add data class to find your meal ui effect

--- app/src/main/java/com/red_velvet/yumhub/ui/fnd_meal/FindYourMaelUiEffect.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/fnd_meal/FindYourMaelUiEffect.kt
@@ -1,7 +1,6 @@
 package com.red_velvet.yumhub.ui.fnd_meal
 
 import com.red_velvet.yumhub.ui.base.BaseUIEffect
-import com.red_velvet.yumhub.ui.search.SearchUIEffect
 
 sealed interface FindYourMaelUiEffect: BaseUIEffect {
     data class ClickOnResultItem(val recipeId: Int) : FindYourMaelUiEffect
