217dd107e9263c1aab8436c1326f92c6079b8fba
ay0o5h <aayosh553@gmail.com>
2023-06-28 23:11:53 +0300

    add loading value lottie file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,4 +104,6 @@ dependencies {
     kapt("com.google.dagger:hilt-android-compiler:2.44")
     //coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    // lottie
+    implementation("com.airbnb.android:lottie:4.2.0")
 }
