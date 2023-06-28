57aaa0c82aacd81e979d5ff17605ffccd7861e3c
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-28 22:07:06 +0300

    add ui effect for empty fields

--- app/src/main/java/com/red_velvet/yumhub/ui/mealsSuggester/MealsSuggesterStep1UiEffect.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/mealsSuggester/MealsSuggesterStep1UiEffect.kt
@@ -7,7 +7,6 @@ sealed interface MealsSuggesterStep1UiEffect : BaseUIEffect {
     data class ClickOnGenderSelector(val gender: String) : MealsSuggesterStep1UiEffect
     data class ClickOnActivityLevelSelector(val activityLevel: Int) : MealsSuggesterStep1UiEffect
     data class OnNextClicked(val type: String) : MealsSuggesterStep1UiEffect
-
     data class ClickOnGoalSelector(val goal: String) : MealsSuggesterStep1UiEffect
-
+    object OnEmptyFields : MealsSuggesterStep1UiEffect
 }
