5059deae4c6344c7b2faaeb14d01d98aeff97a80
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-29 16:28:50 +0300

    pass whole item instead of just id of item

--- app/src/main/java/com/red_velvet/yumhub/ui/mealsSuggester/mealsSuggesterStep3/SuggestedMealsInteractionListener.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/mealsSuggester/mealsSuggesterStep3/SuggestedMealsInteractionListener.kt
@@ -1,7 +1,8 @@
 package com.red_velvet.yumhub.ui.mealsSuggester.mealsSuggesterStep3
 
 import com.red_velvet.yumhub.ui.base.BaseInteractionListener
+import com.red_velvet.yumhub.ui.mealsSuggester.MealsSuggesterStep1UiState
 
 interface SuggestedMealsInteractionListener:BaseInteractionListener {
-    fun onMealClick(id:Int)
+    fun onMealClick(item:MealsSuggesterStep1UiState.SuggestedMeals)
 }
