3340e0fdd521def6b77671b828af4ec18653d9b1
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-29 16:22:27 +0300

    add calories and selected item status to ui state

--- app/src/main/java/com/red_velvet/yumhub/ui/mealsSuggester/MealsSuggesterStep1UiState.kt
+++ app/src/main/java/com/red_velvet/yumhub/ui/mealsSuggester/MealsSuggesterStep1UiState.kt
@@ -14,19 +14,23 @@ data class MealsSuggesterStep1UiState(
     val age: Int? = null,
     val calories: Int? = 0,
     val meals: List<SuggestedMeals>? = emptyList(),
-    val error:ErrorUIState? = null
+    val error:ErrorUIState? = null,
+    val recipeCalories: Int = 0,
 ) : BaseUiState {
     data class SuggestedMeals(
         val id: Int,
         val imageUrl: String = "",
         val title: String = "",
+        val calories: Double = 0.0,
+        var isSelectedRecipe: Boolean = false
     )
 }
 fun RecipeEntity.toSuggestedMeals(): MealsSuggesterStep1UiState.SuggestedMeals{
     return MealsSuggesterStep1UiState.SuggestedMeals(
         imageUrl = imageUrl,
         title = title,
-        id = id
+        id = id,
+        calories = calories
     )
 }
 fun List<RecipeEntity>.toSuggestedMeals():List<MealsSuggesterStep1UiState.SuggestedMeals>{
